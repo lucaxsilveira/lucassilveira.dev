e4b3edbe847ef48985ade5d58665a26c86365b6a
Lucas da Silveira <lucas.hoffmann.lh@gmail.com>
2021-06-26 00:38:12 -0300

    hamburger mobile

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -7,6 +7,7 @@ const Header = (): JSX.Element => {
   const themeKey = '@lucassilveira:theme'
 
   const [darkMode, setDarkMode] = useState(false)
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     if (
@@ -41,6 +42,31 @@ const Header = (): JSX.Element => {
               </span>
             </div>
           </div>
+          <div className="-mr-10 -my-2 md:hidden">
+            <button
+              type="button"
+              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              aria-expanded="false"
+              onClick={() => setOpen(true)}
+            >
+              <span className="sr-only">Open menu</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
           <nav className="dark:text-white hidden md:flex space-x-7 align-middle justify-center">
             <span className="flex items-center">About</span>
             <span className="flex items-center">Projects</span>
@@ -62,6 +88,53 @@ const Header = (): JSX.Element => {
                 onClick={() => setDarkMode(false)}
               />
             )}
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden ${
+          open ? '' : 'hidden'
+        }`}
+      >
+        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+          <div className="pt-5 pb-6 px-5">
+            <div className="flex items-center justify-between">
+              <div className="absolute right-2 top-4 mr-2">
+                <button
+                  type="button"
+                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  onClick={() => setOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div>
+              <nav className="grid gap-y-4">
+                <span className="flex items-center">About</span>
+                <span className="flex items-center">Projects</span>
+                <span className="flex items-center">Find me</span>
+                <button className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium dark:text-white bg-indigo-600 hover:bg-indigo-700">
+                  Resume
+                </button>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ const Home = (): JSX.Element => {
       <div className="container h-screen">
         <div className="flex flex-col justify-center h-full">
           <p className="mb-0 text-primary mono">Oi, me chamo</p>
-          <h1 className="dark:text-white">Lucas da Silveira</h1>
+          <h1 className="dark:text-white text-3xl sm:text-4xl ">
+            Lucas da Silveira
+          </h1>
           <h3 className="subtitle text-gray-400">
             Programador full-stack, apaixonado por front-end.
           </h3>
