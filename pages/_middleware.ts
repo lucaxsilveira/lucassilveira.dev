30d824a2695a0c0c4ac7608037818a61cd51bb41
Lucas da Silveira <lucas.hoffmann.lh@gmail.com>
2021-10-26 23:17:28 -0300

    chore: eslint console log

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,8 @@
 import type { NextFetchEvent } from 'next/server'
 
 export function middleware(request: Request, event: NextFetchEvent) {
+  // eslint-disable-next-line
   console.log('Hello ' + request.url)
+  // eslint-disable-next-line
   console.log('event', event)
 }
